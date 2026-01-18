e7dd5e8e15b2ac9c61660c8d5d658c4397ccba0d
Nikhil-Kumawat369 <coder6185@gmail.com>
2026-01-18 20:58:53 +0530

    Added Few comments to it

--- generator.py
+++ generator.py
@@ -6,9 +6,11 @@ num = int(input("Till How Many Number You Want To Write if / elif To Check Odd o
 # This make a input logic in the generated file 
 f.write("num = int(input('Enter the number you want to check Odd or Even :- '))\n\n")
 
+# This prints the initial "if" statement
 f.write("if num == 1 :\n")
 f.write("  print('Odd')\n")
 
+# This loop prints the remaining "elif" statements
 for i in range(2 , num+1) :
     
     if i % 2 == 0 :
@@ -19,6 +21,7 @@ for i in range(2 , num+1) :
         f.write(f"elif num == {i} :\n")
         f.write(f"  print('Odd')\n")
 
+# This prints the default "else" statement
 f.write("else :\n")
 f.write(f"   print('Hey , I was only trained for numbers up to {num}. I cant handle above it.')")
 
